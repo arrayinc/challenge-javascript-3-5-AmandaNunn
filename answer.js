1de51fd97c3f63fc533e6aa47da9f04feb294aac
Amanda Nunn <amandanunn@thearrayschool.com>
2018-12-13 15:47:05 -0700

    final answer

--- answer.js
+++ answer.js
@@ -1,1 +1,25 @@
 // *** YOUR ANSWER BELOW ***
+function drawBox(h, w){
+  let str = "";
+  for(let i = 1; i <= h; i++){
+
+    if(i === 1 || i === h){
+      for( let j = 1; j <= w; j++){
+        if(j === 1 || j === w){
+          str += "+";
+        } else{ str += "-";}
+      }
+    } else{
+      for(let k = 1; k <= w; k++)
+      if(k === 1 || k === w){
+        str += "|";
+      } else {
+        str += " ";
+      }
+    }
+    str += "\n";
+  }
+  return str;
+}
+
+drawBox(10, 10);
